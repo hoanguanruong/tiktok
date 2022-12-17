1c9da7f97e027b46454153385b2280164e901d72
Truong <cdcntt18truong@gmail.com>
2022-12-18 01:53:46 +0700

    header tiktok #1

--- src/components/Layout/Components/Header/index.js
+++ src/components/Layout/Components/Header/index.js
@@ -1,18 +1,37 @@
-import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
-import images from '~/assets/images';
+import classNames from 'classnames/bind'; // Thư viện dùng để đặt tên class có dấu - vì trong jsx không đặt được tên có dấu -
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-const cx = classNames.bind(styles);
-console.log(images.logo);
+import styles from './Header.module.scss'; // Load module scss của nó ra
+import images from '~/assets/images'; // Load Hình ảnh từ folder ra
+
+const cx = classNames.bind(styles); //Khai báo cx dùng để chạy classNames
+
 function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
         {/* logo */}
-        {/* <div className={cx('logo')}>{images.logo}</div> */}
-        <img src={images.logo} alt="tiktok" />
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="tiktok" />
+        </div>
         {/* search */}
-        {/* tool */}
+        <div className={cx('search')} spellCheck={false}>
+          <input placeholder="Tìm kiếm tài khoản và video" />
+
+          <button className={cx('clear')}>
+            {/* Clear */}
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          {/* Loading */}
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search_btn')}>
+            {/* Search */}
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        {/* Action */}
+        <div className={cx('action')}></div>
       </div>
     </header>
   );
