9f8e1fefc041f447fb595b3840643378f1481cbb
Truong <cdcntt18truong@gmail.com>
2022-12-21 03:46:08 +0700

    fix fix

--- src/components/Layout/Components/Header/index.js
+++ src/components/Layout/Components/Header/index.js
@@ -3,49 +3,49 @@ import classNames from 'classnames/bind'; // Thư viện dùng để đặt tên
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless'; // Giống như tooltip
-import styled from 'styled-components';
-import { useSpring, motion } from 'framer-motion';
+// import styled from 'styled-components';
+// import { useSpring, motion } from 'framer-motion';
 // import { Box, AnimationToolTip } from '~/Animation/AnimationToolTip';
-
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss'; // Load module scss của nó ra
 import images from '~/assets/images'; // Load Hình ảnh từ folder ra
 
 const cx = classNames.bind(styles); //Khai báo cx dùng để chạy classNames
-const Box = styled(motion.div)`
-  background: #333;
-  color: white;
-  padding: 5px 10px;
-  border-radius: 4px;
-`;
+// const Box = styled(motion.div)`
+//   background: #333;
+//   color: white;
+//   padding: 5px 10px;
+//   border-radius: 4px;
+// `;
 function Header() {
   const [searchResult, setsearchResult] = useState([]);
 
   useEffect(() => {
     setTimeout(() => {
       setsearchResult([1, 2, 3]);
-    }, 3000);
+    }, 300);
   }, []);
-  const springConfig = { damping: 15, stiffness: 300 };
-  const initialScale = 0.5;
-  const opacity = useSpring(0, springConfig);
-  const scale = useSpring(initialScale, springConfig);
+  // const springConfig = { damping: 15, stiffness: 300 };
+  // const initialScale = 0.5;
+  // const opacity = useSpring(0, springConfig);
+  // const scale = useSpring(initialScale, springConfig);
 
-  function onMount() {
-    scale.set(1);
-    opacity.set(1);
-  }
+  // function onMount() {
+  //   scale.set(1);
+  //   opacity.set(1);
+  // }
 
-  function onHide({ unmount }) {
-    const cleanup = scale.onChange((value) => {
-      if (value <= initialScale) {
-        cleanup();
-        unmount();
-      }
-    });
+  // function onHide({ unmount }) {
+  //   const cleanup = scale.onChange((value) => {
+  //     if (value <= initialScale) {
+  //       cleanup();
+  //       unmount();
+  //     }
+  //   });
 
-    scale.set(initialScale);
-    opacity.set(0);
-  }
+  //   scale.set(initialScale);
+  //   opacity.set(0);
+  // }
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
@@ -58,13 +58,15 @@ function Header() {
           interactive
           visible={searchResult.length > 0}
           render={(attrs) => (
-            <Box style={{ scale, opacity }} className={cx('search_result')} tabIndex="-1" {...attrs}>
-              Kết Quả
-            </Box>
+            <PopperWrapper>
+              <div /*style={{ scale, opacity }}*/ className={cx('search_result')} tabIndex="-1" {...attrs}>
+                Kết Quả
+              </div>
+            </PopperWrapper>
           )}
-          animation={true}
-          onMount={onMount}
-          onHide={onHide}
+          // animation={true}
+          // onMount={onMount}
+          // onHide={onHide}
         >
           <div className={cx('search')} spellCheck={false}>
             <input placeholder="Tìm kiếm tài khoản và video" />
